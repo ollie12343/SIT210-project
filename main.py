8f8e117b1a2bee023d02c692842cd59425e49388
ollie12343 <87453519+ollie12343@users.noreply.github.com>
2024-06-04 00:15:50 +1000

    final python file changes

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from paho.mqtt import client as mqtt_client
 import random
 import time
+import RPi.GPIO as GPIO
+import threading
+
+
+#pins
+buzzer = 8
+button = 10
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(buzzer, GPIO.OUT)
+GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 
 
@@ -10,8 +21,8 @@ topic = "python/mqtt/ollie"
 # Generate a Client ID with the publish prefix.
 pub_client_id = f'publish-{random.randint(0, 1000)}'
 sub_client_id = f'sub-{random.randint(0, 1000)}'
-# username = 'emqx'
-# password = 'public'
+
+
 
 def connect_mqtt(client_id):
     def on_connect(client, userdata, flags, rc):
@@ -27,33 +38,53 @@ def connect_mqtt(client_id):
     return client
 
 
-def publish(client):
-    msg_count = 1
-    while True:
+def publish(client, msg):
+	
+    for i in range(2):
         time.sleep(1)
-        msg = f"messages: {msg_count}"
         result = client.publish(topic, msg)
-        # result: [0, 1]
         status = result[0]
         if status == 0:
             print(f"Send `{msg}` to topic `{topic}`")
         else:
             print(f"Failed to send message to topic {topic}")
-        msg_count += 1
-        if msg_count > 5:
-            break
         
         
 def subscribe(client: mqtt_client):
     def on_message(client, userdata, msg):
-        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+        if msg.payload.decode() == "On" and msg.topic == "python/mqtt/ollie":
+        	print("On")
+        	GPIO.output(buzzer, GPIO.HIGH)
+        	time.sleep(1)
+        	GPIO.output(buzzer, GPIO.LOW)
+
 
     client.subscribe(topic)
     client.on_message = on_message
 
 
 if __name__ == '__main__':
-    
+
     sub = connect_mqtt(sub_client_id)
     subscribe(sub)
-    sub.loop_forever()
+    
+    t1 = threading.Thread(target=sub.loop_forever)
+
+    	
+    t1.start()
+    toggle = False
+    while True:
+    	if GPIO.input(button) == GPIO.HIGH:
+    		if not toggle:
+    			publish(connect_mqtt(pub_client_id), "Unlock")
+    			toggle = True
+    		else:
+    			publish(connect_mqtt(pub_client_id), "Lock")
+    			toggle = False
+    		
+    t1.join()
+    
+    
+
+    
+    GPIO.cleanup()
